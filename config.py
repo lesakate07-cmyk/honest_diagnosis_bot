93e80ed39efa3aca7153981932ac9a9245fb66ee
lesakate07-cmyk <lesakate07@gmail.com>
2026-02-11 15:51:42 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "8482171248:AAGXOWQBFTj7nBy1sdVXac6Xd9ryZY5d47I"
+
+GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfeO1aLNGb91QJ-HHC0U0O72Bj94q2AHUJYTHqqnd19TBBBtw/viewform?usp=publish-editor"
