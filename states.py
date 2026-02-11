cb4fb810222e505ae78480908b121baf742275d0
lesakate07-cmyk <lesakate07@gmail.com>
2026-02-11 15:56:02 +0300

    states.py

---
+++ states.py
@@ -0,0 +1,10 @@
+from aiogram.fsm.state import StatesGroup, State
+
+class TestState(StatesGroup):
+    q1 = State()
+    q2 = State()
+    q3 = State()
+    q4 = State()
+    q5 = State()
+    q6 = State()
+    q7 = State()
